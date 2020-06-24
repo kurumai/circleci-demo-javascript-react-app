c5ac133c15c7c4af522e3f1ce26a3c1d7c45c8c2
Noboru Kurumai <noboru@circleci.com>
2020-06-24 15:30:00 +0900

    change photos

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
 
   // Baby Hippo Gram
   // National Park Gram
-  title = '"Baby Hippo Gram"';
+  title = '"National Park Gram"';
 
   state = {
       cards: []
@@ -29,7 +29,7 @@ class App extends Component {
   componentDidMount() {
     // https://www.reddit.com/r/babyhippos/hot/.json?count=20
     // https://www.reddit.com/r/NationalPark/hot/.json?count=20
-      fetch('https://www.reddit.com/r/babyhippos/hot/.json?count=20')
+      fetch('https://www.reddit.com/r/NationalPark/hot/.json?count=20')
       .then(res => res.json())
       .then((data) => {
           console.log(data.data.children);
